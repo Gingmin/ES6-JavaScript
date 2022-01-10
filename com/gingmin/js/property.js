99b58e5a94f86355e6bd4c7afe64e634103d829e
gingmin <kkmk220@gmail.com>
2022-01-10 23:49:38 +0900

    220110-h

--- com/gingmin/js/property.js
+++ com/gingmin/js/property.js
@@ -15,12 +15,57 @@
 //
 // console.log(JSON.stringify(descriptor, null, 2));
 
+// let user = {
+//    name: "John"
+// };
+//
+// Object.defineProperty(user, "name", {
+//    writable: false
+// });
+//
+// user.name = "Pete";
+
+// let user = {
+// //    name: "John",
+// //    toString() {
+// //       return this.name;
+// //    }
+// // };
+// // Object.defineProperty(user, "toString", {
+// //    enumerable : false
+// // });
+// //
+// // for(let key in user) console.log(key);
+
+// let user = {
+//    name: "John",
+//    surname: "Smith",
+//
+//    get fullName() {
+//       return `${this.name} ${this.surname}`;
+//    },
+//
+//    set fullName(value) {
+//       [this.name, this.surname] = value.split(" ");
+//    }
+// };
+// user.fullName = "Alice Cooper";
+// console.log(user.name);
+// console.log(user.surname);
+
 let user = {
-   name: "John"
+   get name() {
+      return this._name;
+   },
+
+   set name(value) {
+      if(value.length < 4) {
+         console.log("short!");
+         return;
+      }
+      this._name = value;
+   }
 };
-
-Object.defineProperty(user, "name", {
-   writable: false 
-});
-
 user.name = "Pete";
+console.log(user.name);
+user.name = "";
