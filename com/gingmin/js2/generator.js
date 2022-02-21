64dd047ccc2faf2c786986c4b74b2b79ca1f2bf9
gingmin <kkmk220@gmail.com>
2022-02-21 21:31:58 +0900

    220221-4

--- com/gingmin/js2/generator.js
+++ com/gingmin/js2/generator.js
@@ -14,4 +14,24 @@ function* generateSequence() {
 // alert(JSON.stringify(three));
 
 let sequence = [0, ...generateSequence()];
-alert(sequence);
+// alert(sequence);
+
+let range = {
+    from: 1,
+    to: 5,
+
+    [Symbol.iterator]() {
+        return {
+            current: this.from,
+            last: this.to,
+            next() {
+                if (this.current <= this.last) {
+                    return { done: false, value: this.current++ };
+                } else {
+                    return { done: true};
+                }
+            }
+        };
+    }
+};
+alert([...range]);
