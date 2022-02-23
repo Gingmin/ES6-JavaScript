36c2091c0fda6c7643f4d3cb7298dccbf1b7405c
gingmin <kkmk220@gmail.com>
2022-02-23 23:19:29 +0900

    220223

--- com/gingmin/js2/generator.js
+++ com/gingmin/js2/generator.js
@@ -13,26 +13,70 @@ function* generateSequence() {
 // let three = generator.next();
 // alert(JSON.stringify(three));
 
-let sequence = [0, ...generateSequence()];
-// alert(sequence);
+// let sequence = [0, ...generateSequence()];
+// // alert(sequence);
+//
+// let range = {
+//     from: 1,
+//     to: 5,
+//
+//     [Symbol.iterator]() {
+//         return {
+//             current: this.from,
+//             last: this.to,
+//             next() {
+//                 if (this.current <= this.last) {
+//                     return { done: false, value: this.current++ };
+//                 } else {
+//                     return { done: true};
+//                 }
+//             }
+//         };
+//     }
+// };
 
-let range = {
-    from: 1,
-    to: 5,
+// let range = {
+//     from: 1,
+//     to: 5,
+//     *[Symbol.iterator]() {
+//         for (let value = this.from; value <= this.to; value++) {
+//             yield value;
+//         }
+//     }
+// }
+// alert([...range]);
 
-    [Symbol.iterator]() {
-        return {
-            current: this.from,
-            last: this.to,
-            next() {
-                if (this.current <= this.last) {
-                    return { done: false, value: this.current++ };
-                } else {
-                    return { done: true};
-                }
-            }
-        };
+// function* generateSequence(start, end) {
+//     for (let i = start; i <= end; i++) yield i;
+// }
+// function* generatePasswordCodes() {
+//     yield* generateSequence(48, 57);
+//     yield* generateSequence(65, 90);
+//     yield* generateSequence(97, 122);
+// }
+// let str = "";
+// for (let code of generatePasswordCodes()) {
+//     str += String.fromCharCode(code);
+// }
+// alert(str);
+
+// function* gen() {
+//     let result = yield "2 + 2 = ?";
+//     alert(result);
+// }
+// let generator = gen();
+// let question = generator.next().value;
+// alert(question);
+// generator.next(4);
+
+function* gen() {
+    try {
+        let result = yield "2 + 2 = ?"; // (1)
+        alert("위에서 에러가 던져졌기 때문에 실행 흐름은 여기까지 다다르지 못합니다.");
+    } catch(e) {
+        alert(e); // 에러 출력
     }
-};
-
-alert([...range]);
+}
+let generator = gen();
+let question = generator.next().value;
+generator.throw(new Error("데이터베이스에서 답을 찾지 못했습니다.")); // (2)
